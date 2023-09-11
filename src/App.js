5d40796078348cda34e3702b768cd9ede76bf4b1
Julius Gyeti 10989434 <jgyeti@st.ug.edu.gh>
2023-09-10 17:49:39 -1200

    Added the header component and link to the shop page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import React from 'react';
 import HomePage from './pages/homepage/Homepage.component';
 import { Route, Routes } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from '../src/components/header/header-component'
 
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
-        <Route exact path='/' Component={HomePage}/>
-        <Route path='/shop' Component={ShopPage}/>
+        <Route exact path='/' Component={HomePage} />
+        <Route path='/shop' Component={ShopPage} />
       </Routes>
     </div>
   );
